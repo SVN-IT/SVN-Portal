f981981916158369b2978813c0b9e7828285bc9e
datp1044 <datp1044@gmail.com>
2025-03-18 11:11:05 +0700

    update code

---
+++ AutoCheckService/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SVNShareLib;
+
+string BaseURL = "http://10.10.99.10:8101/";
+string GetAndUploadProductionResultDataURL = "api/DB/GetAndUploadProductionResultData";
+
+HttpClientHelper<BODataProcessResult> httpClientHelper = new HttpClientHelper<BODataProcessResult>(BaseURL, 1000);
+BODataProcessResult bODataProcessResult = new BODataProcessResult();
+var result = await httpClientHelper.PostRequest(GetAndUploadProductionResultDataURL, bODataProcessResult, new CancellationToken(false));
+Console.WriteLine("[Time]: " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss") + " [Status]: " + result.OK.ToString() + " [Message]: " + result.Message);
