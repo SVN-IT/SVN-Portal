09e72fa3be374e9e98ce789fe14cf6b75ec55cf9
datp1044 <datp1044@gmail.com>
2025-05-27 17:23:22 +0700

    update code

---
+++ SVNShareLib/DTO/SVN_Label_InfoUI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVNShareLib.DTO
+{
+    public class SVN_Label_InfoUI
+    {
+        public string Date { get; set; }
+        public string LotID { get; set; }
+        public string SerialNumbers { get; set; }
+        public DateTime ScanDateTime { get; set; }
+        public string Status { get; set; }
+        public string Operation { get; set; }
+        public string EmployerID { get; set; }
+    }
+}
