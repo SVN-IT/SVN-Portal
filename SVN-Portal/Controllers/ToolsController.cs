95263d0a3fa4ef66093389d9ad284744cc590274
datp1044 <datp1044@gmail.com>
2025-05-29 13:44:59 +0700

    update code

--- SVN-Portal/Controllers/ToolsController.cs
+++ SVN-Portal/Controllers/ToolsController.cs
@@ -393,7 +393,7 @@ namespace SVN_Portal.Controllers
                             }
                             else
                             {
-                                processResult.OK = false;
+                                processResult.OK = true;
                                 processResult.Message = "In nhãn thành công và đã lưu thông tin vào cơ sở dữ liệu.";
                             }
                         }
@@ -408,9 +408,10 @@ namespace SVN_Portal.Controllers
             }
             catch (Exception ex)
             {
+                processResult.OK = false;
                 processResult.Message = ex.Message;
             }
-            return Json(new { palletID = requestPayload.PalletID, message = processResult.Message });
+            return Json(new { result = processResult.OK, palletID = requestPayload.PalletID, message = processResult.Message });
         }
 
         [HttpPost]
@@ -428,57 +429,64 @@ namespace SVN_Portal.Controllers
                 requestPayload.Quantity = labelConfiguration.Quantity.ToString();
                 requestPayload.LotID = labelConfiguration.LotID;
 
-                PrinterConfigData printerConfigData = new PrinterConfigData();
-                printerConfigData = await printerDataPortal.ReadByID(requestPayload.PrinterID);
-
-                List<SVN_Label_InfoUI> dataUI = new List<SVN_Label_InfoUI>();
-                dataUI = sVN_Label_InfoDataPortal.ReadListByPalletID(requestPayload.PalletID);
-                if (dataUI != null && dataUI.Count > 0)
+                if(!string.IsNullOrWhiteSpace(requestPayload.PalletID))
                 {
-                    List<SVN_Label_InfoUI> top50Item1 = new List<SVN_Label_InfoUI>();
-                    List<SVN_Label_InfoUI> top50Item2 = new List<SVN_Label_InfoUI>();
-                    List<SVN_Label_InfoUI> top50Item3 = new List<SVN_Label_InfoUI>();
+                    PrinterConfigData printerConfigData = new PrinterConfigData();
+                    printerConfigData = await printerDataPortal.ReadByID(requestPayload.PrinterID);
 
-                    List<SVN_Label_InfoUI> remainItems = new List<SVN_Label_InfoUI>();
-
-                    top50Item1 = dataUI.Take(50).ToList();
-                    if(top50Item1 != null && top50Item1.Count > 0)
+                    List<SVN_Label_InfoUI> dataUI = new List<SVN_Label_InfoUI>();
+                    dataUI = sVN_Label_InfoDataPortal.ReadListByPalletID(requestPayload.PalletID);
+                    if (dataUI != null && dataUI.Count > 0)
                     {
-                        remainItems = dataUI.Except(top50Item1).ToList();
-                        if (remainItems != null && remainItems.Count > 0) 
+                        List<SVN_Label_InfoUI> top50Item1 = new List<SVN_Label_InfoUI>();
+                        List<SVN_Label_InfoUI> top50Item2 = new List<SVN_Label_InfoUI>();
+                        List<SVN_Label_InfoUI> top50Item3 = new List<SVN_Label_InfoUI>();
+
+                        List<SVN_Label_InfoUI> remainItems = new List<SVN_Label_InfoUI>();
+
+                        top50Item1 = dataUI.Take(50).ToList();
+                        if (top50Item1 != null && top50Item1.Count > 0)
                         {
-                            top50Item2 = remainItems.Take(50).ToList();
-                            top50Item3 = remainItems.Except(top50Item2).ToList();
+                            remainItems = dataUI.Except(top50Item1).ToList();
+                            if (remainItems != null && remainItems.Count > 0)
+                            {
+                                top50Item2 = remainItems.Take(50).ToList();
+                                top50Item3 = remainItems.Except(top50Item2).ToList();
+                            }
+                        }
+
+                        requestPayload.Print150Seri = true;
+                        requestPayload.AllSeri1 = "";
+                        requestPayload.AllSeri2 = "";
+                        requestPayload.AllSeri3 = "";
+                        requestPayload.Quantity = dataUI.Count.ToString();
+
+                        if (top50Item1 != null && top50Item1.Count > 0)
+                        {
+                            requestPayload.AllSeri1 = string.Join("", top50Item1.Select(item => item.SerialNumbers));
+                        }
+                        if (top50Item2 != null && top50Item2.Count > 0)
+                        {
+                            requestPayload.AllSeri2 = string.Join("", top50Item2.Select(item => item.SerialNumbers));
+                        }
+                        if (top50Item3 != null && top50Item3.Count > 0)
+                        {
+                            requestPayload.AllSeri3 = string.Join("", top50Item3.Select(item => item.SerialNumbers));
                         }
                     }
 
-                    requestPayload.Print150Seri = true;
-                    requestPayload.AllSeri1 = "";
-                    requestPayload.AllSeri2 = "";
-                    requestPayload.AllSeri3 = "";
-                    requestPayload.Quantity = dataUI.Count.ToString();
-
-                    if (top50Item1 != null && top50Item1.Count > 0)
-                    {
-                        requestPayload.AllSeri1 = string.Join("", top50Item1.Select(item => item.SerialNumbers));
-                    }
-                    if (top50Item2 != null && top50Item2.Count > 0)
-                    {
-                        requestPayload.AllSeri2 = string.Join("", top50Item2.Select(item => item.SerialNumbers));
-                    }
-                    if (top50Item3 != null && top50Item3.Count > 0)
-                    {
-                        requestPayload.AllSeri3 = string.Join("", top50Item3.Select(item => item.SerialNumbers));
-                    }
+                    processResult = toolsHelper.PrintToastLabelByTCP(requestPayload, printerConfigData);
+                    
                 }
 
-                processResult = toolsHelper.PrintToastLabelByTCP(requestPayload, printerConfigData);
+                
             }
             catch (Exception ex)
             {
+                processResult.OK = false;
                 processResult.Message = ex.Message;
             }
-            return Json(new { message = processResult.Message });
+            return Json(new { result = processResult.OK, message = processResult.Message });
         }
 
         [HttpPost]
