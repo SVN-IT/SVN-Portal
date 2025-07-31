1b1375b627978313b89d5e5ce204f9eb8242800c
datp1044 <datp1044@gmail.com>
2025-07-31 11:57:13 +0700

    update code

---
+++ ViidooDBServiceAPI/Controllers/ViindooConnectController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViidooDBServiceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ViindooConnectController : Controller
+    {
+        [Route("GetDataFromViindoo")]
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
