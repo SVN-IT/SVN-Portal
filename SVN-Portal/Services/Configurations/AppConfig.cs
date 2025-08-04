cdcafe7d6ab9bd447c987db8fdb9e4e96883b797
datp1044 <datp1044@gmail.com>
2025-08-04 09:30:44 +0700

    update code

--- SVN-Portal/Services/Configurations/AppConfig.cs
+++ SVN-Portal/Services/Configurations/AppConfig.cs
@@ -4,7 +4,7 @@
     {
         public string ProductMode { get; set; }
         public string MasterOperList { get; set; }
-        public string OperList { get; set; }
+        public string OperList { get; set; } //không dùng nữa
         public int TimeBeforeReload { get; set; }
         public int ChartCol { get; set; }
         public string ShowSingleChart { get; set; }
