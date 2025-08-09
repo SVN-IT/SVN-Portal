cbb48789d9e3f4df172cfbdf0f9ced157ab1706f
datp1044 <datp1044@gmail.com>
2025-08-09 11:36:13 +0700

    update code

--- SVN-Portal/Controllers/HomeController.cs
+++ SVN-Portal/Controllers/HomeController.cs
@@ -545,6 +545,39 @@ namespace SVN_Portal.Controllers
                 var dataPortal = new SVN_production_resultDataPortal(connectionString);
                 model = await dataPortal.GetDataByOperAndWC(date, operInfo, storedProceduce, tableName, dBConfiguration.CheckListConnectionString);
 
+                string pdChecked = "🔴";
+                string mtChecked = "🔴";
+                string qcChecked = "🔴";
+                string pdConfirmed = "🔴";
+                string qcConfirmed = "🔴";
+
+                if (model.IsPDChecked)
+                {
+                    pdChecked = "🟢";
+                }
+                if (model.IsMTChecked)
+                {
+                    mtChecked = "🟢";
+                }
+                if (model.IsQCChecked)
+                {
+                    qcChecked = "🟢";
+                }
+                if (model.IsPDConfirmed)
+                {
+                    pdConfirmed = "🟢";
+                }
+                if (model.IsQCConfirmed)
+                {
+                    qcConfirmed = "🟢";
+                }
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<p style='font-size:20px' class=' text-light'>");
+                sb.Append("<strong>Checklist status</strong>: ");
+                sb.Append(pdChecked + " PD - " + mtChecked + " MT - " + qcChecked + " QC Checked | " + pdConfirmed + " PD - " + qcConfirmed + " QC Confirmed");
+                sb.Append("</p>");
+
                 //sử dụng stringBuilder để build lại 2 table
                 if (model != null)
                 {
@@ -567,6 +600,7 @@ namespace SVN_Portal.Controllers
                         woRunning = model.WORunning,
                         product = model.Product,
                         customer = model.Customer,
+                        checklistStatus = sb.ToString(),
                         isProduction = model.IsProduction,
                         canProduction = model.CanProduction
                     });
