66a8f6ba45341e22b787c3dd671510b18765287d
datp1044 <datp1044@gmail.com>
2025-11-10 13:53:23 +0700

    update code

--- SVN-Portal/DAL/DataPortal/SVN_production_resultDataPortal.cs
+++ SVN-Portal/DAL/DataPortal/SVN_production_resultDataPortal.cs
@@ -153,10 +153,10 @@ namespace SVN_Portal.DAL.DataPortal
                         QtyProdResultViewModel val5 = new QtyProdResultViewModel();
 
                         val1.Time = "8h-10h";
-                        val2.Time = "10h-11h30";
-                        val3.Time = "12h30-15h";
-                        val4.Time = "15h10-17h30";
-                        val5.Time = "18h-20h";
+                        val2.Time = "10h-12h";
+                        val3.Time = "13h30-15h30";
+                        val4.Time = "15h30-17h30";
+                        val5.Time = "18h30-21h";
 
                         viewModel.MasterOperation = item.MasterOperation;
                         viewModel.Operation = item.Operation;
@@ -544,10 +544,10 @@ namespace SVN_Portal.DAL.DataPortal
                         QtyProdResultViewModel val5 = new QtyProdResultViewModel();
 
                         val1.Time = "8h-10h";
-                        val2.Time = "10h-11h30";
-                        val3.Time = "12h30-15h";
-                        val4.Time = "15h10-17h30";
-                        val5.Time = "18h-20h";
+                        val2.Time = "10h-12h";
+                        val3.Time = "13h30-15h30";
+                        val4.Time = "15h30-17h30";
+                        val5.Time = "18h30-21h";
 
                         viewModel.MasterOperation = item.MasterOperation;
                         viewModel.Operation = item.Operation;
@@ -934,10 +934,10 @@ namespace SVN_Portal.DAL.DataPortal
                         QtyProdResultViewModel val5 = new QtyProdResultViewModel();
 
                         val1.Time = "8h-10h";
-                        val2.Time = "10h-11h30";
-                        val3.Time = "12h30-15h";
-                        val4.Time = "15h10-17h30";
-                        val5.Time = "18h-20h";
+                        val2.Time = "10h-12h";
+                        val3.Time = "13h30-15h30";
+                        val4.Time = "15h30-17h30";
+                        val5.Time = "18h30-21h";
 
                         viewModel.MasterOperation = item.MasterOperation;
                         viewModel.Operation = item.Operation;
@@ -1390,9 +1390,9 @@ namespace SVN_Portal.DAL.DataPortal
 
                         val1.Time = "8h-10h";
                         val2.Time = "10h-12h";
-                        val3.Time = "13h-15h";
-                        val4.Time = "15h10-17h30";
-                        val5.Time = "18h-20h";
+                        val3.Time = "13h30-15h30";
+                        val4.Time = "15h30-17h30";
+                        val5.Time = "18h30-21h";
 
                         viewModel.Operation = item.Operation;
                         viewModel.Name = item.Name;
@@ -1755,10 +1755,10 @@ namespace SVN_Portal.DAL.DataPortal
                     QtyProdResultViewModel val5 = new QtyProdResultViewModel();
 
                     val1.Time = "8h-10h";
-                    val2.Time = "10h-11h30";
-                    val3.Time = "12h30-15h";
-                    val4.Time = "15h10-17h30";
-                    val5.Time = "18h-20h";
+                    val2.Time = "10h-12h";
+                    val3.Time = "13h30-15h30";
+                    val4.Time = "15h30-17h30";
+                    val5.Time = "18h30-21h";
 
                     viewModel.Operation = oper.Operation;
                     
