2c5a77fc21f3085f13b3e0637b6ad05c94ccfc31
datp1044 <datp1044@gmail.com>
2025-11-11 14:15:40 +0700

    update: Data Portal cho downtime new

---
+++ SVN-Portal/DAL/DTO/SVN_ACT_downtimeUI.cs
@@ -0,0 +1,17 @@
+﻿namespace SVN_Portal.DAL.DTO
+{
+    public class SVN_ACT_downtimeUI
+    {
+        public string OperationName { get; set; }
+        public string WorkDate { get; set; }
+        public DateTime? StopTime { get; set; }
+        public DateTime? RunTime { get; set; }
+        public DateTime? first_finish_date_time { get; set; }
+        public DateTime? last_finish_date_time { get; set; }
+        public DateTime? adjusted_first_finish_date_time { get; set; }
+        public double LanDauTien { get; set; }
+        public double KhongPhaiLanDau { get; set; }
+        public double TongDowntime { get; set; }
+
+    }
+}
