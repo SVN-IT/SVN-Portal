f4eca3820cc14510fb44bcd674f4cc470bb99148
datp1044 <datp1044@gmail.com>
2025-11-11 14:00:25 +0700

    Update code: Update SVN_ACT_downtime Data Portal dùng cho tính downtime mới

---
+++ SVN-Portal/DAL/DataPortal/SVN_ACT_downtimeDataPortal.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using SVN_Portal.DAL.DTO;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SVN_Portal.DAL.DataPortal
+{
+    public class SVN_ACT_downtimeDataPortal
+    {
+        string connectionString;
+        public SVN_ACT_downtimeDataPortal(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public async Task<List<SVN_ACT_downtimeUI>> ReadList(string date, string storedProceduce = "SVN_ACT_downtime")
+        {
+            List<SVN_ACT_downtimeUI> dataUI = new List<SVN_ACT_downtimeUI>();
+            int timeOut = 1000;
+            try
+            {
+                using (IDbConnection conn = new SqlConnection(connectionString))
+                {
+                    //Gọi thủ tục tính toán kết quả theo tarhet
+                    string storedProcedure = storedProceduce;
+                    DynamicParameters parameters = new DynamicParameters();
+
+                    var datas = await conn.QueryAsync<SVN_ACT_downtimeUI>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+                    return datas.ToList();
+                }
+                return dataUI;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
