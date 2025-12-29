737221405d1cce603c23be2d7f84baae6e9f9927
datp1044 <datp1044@gmail.com>
2025-12-29 09:50:30 +0700

    update code

---
+++ SVN-Portal/Models/CostRevenueViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SVN_Portal.Models
+{
+    public class CostRevenueViewModel
+    {
+        public string Time { get; set; }
+        public string StrDateTime { get; set; }
+        public string Key { get; set; }
+        public double Value { get; set; }
+    }
+}
